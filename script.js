ec79bbe553dd4d40bd7af0a8af4995c4fd2dfb0f
Eliasv10 <150226500+Eliasv10@users.noreply.github.com>
2024-05-22 02:32:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+// if(5<10){
+//     console.log(" 5 is less")
+// }
+// else{
+//     console.log("10 is greater")
+// }
+
+// let age=10;
+// if( age<18){
+//     console.log("You are not Elegible for lisence")
+// }
+// else{
+//     console.log("you are  eligible")
+// }
+
+// let time=1;
+// if (time<=12) {
+//     console.log("Good Morning")
+// }  
+// else if (time>12 && time<=15) {
+//     console.log("Good Afternoon")
+// }
+// else if (time>15 && time<=19) {
+//     console.log("Good Evening")
+// }
+// else {
+//     console.log("Good Night")
+// }
+
+// let a = 18;
+// (a<20)?console.log("a is less than 20"):console.log("a is greater than 20")
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+
+// for(let i=1;i<=5;i++){
+//     console.log(i)
+// }
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// let fruits=["apple","orange","grapes","banana"]
+//     for(let i=0;i<fruits.length;i++){
+
+//         console.log(fruits[i])
+
+//     }
+
+// let number=[1,2,3,4,5,6];
+// for(i=0;i<number.length;i++){
+//     if(number[i]%2==0){
+//         console.log(`${number[i]}is even`)
+    
+// }
+// else{
+//     console.log(`${number[i]}is odd`)
+
+// }
+// }
+
+// find the sum of the array
+// let values =[1,2,3,4,5,6,7,8,9]
+// let sum =0;
+// for(let i= 0;i<values.length;i++){
+//     sum += values[i];
+// }
+
+// console.log("sum is " + sum)
+
+// multiplication table
+for(let i =1; i<5; i++){
+    for(j=1; j<=5;j++){
+        console.log(`${i} * ${j} = ${i * j} `)
+    }
+}
